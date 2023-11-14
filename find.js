ca5f72d5b75fac77fa8f88868f5ba1a40c593843
FaizAlam4 <faizalam4e@gmail.com>
2023-11-14 12:42:34 +0530

    little bug fixed in find.js

--- find.js
+++ find.js
@@ -1,8 +1,6 @@
 export function find(element, cb) {
-  if (element == null) return undefined;
-
   for (let ele of element) {
     if (cb(ele)) return ele;
   }
-  return false;
+  return undefined;
 }
